1be8794013eb2a251164e60bafff8a6c97ef5058
kienonline19 <kienonline19.haui@gmail.com>
2025-10-02 11:50:30 +0700

    Add project files.

---
+++ MVC_Demo/Controllers/GreenwichController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Demo.Models;
+
+namespace MVC_Demo.Controllers
+{
+    public class GreenwichController : Controller
+    {
+        public IActionResult Index()
+        {
+            CampusInfo model = new CampusInfo
+            {
+                Campus = "Greenwich Vietnam",
+                Department = "Computing Department",
+                Course = "Advanced Microservices",
+                CourseCode = "AMD201"
+            };
+            return View(model);
+        }
+
+        public IActionResult Hanoi()
+        {
+            //int year = DateTime.Now.Year;
+
+            ////ViewBag.Year = year;
+            //ViewData["Year"] = year;
+            //ViewBag.Campus = "Hanoi Campus";
+
+            CampusInfo model = new CampusInfo
+            {
+                Year = DateTime.Now.Year,
+                Campus = "Hanoi Campus",
+                Department = "Computing Department"
+            };
+
+            return View(model);
+        }
+
+        public IActionResult HCMC()
+        {
+            CampusInfo mdl = new CampusInfo
+            {
+                Year = DateTime.Now.Year,
+                Campus = "Ho Chi Minh Campus",
+                Department = "Business Department",
+                Course = "Digital Marketing",
+                CourseCode = "DM301"
+            };
+
+            return View(mdl);
+        }
+    }
+}
---
+++ MVC_Demo/Models/CampusInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Demo.Models
+{
+    public class CampusInfo
+    {
+        public int Year { get; set; }
+        public string Campus { get; set; }
+        public string Department { get; set; }
+        public string Course { get; set; }
+        public string CourseCode { get; set; }
+    }
+}
